1a17b2aca776ad0386f7ae80adbaba2f541e6a16
pvz01 <81989793+pvz01@users.noreply.github.com>
2021-05-17 13:59:10 -0500

    Update deepinstinctagentless.py

--- deepinstinctagentless.py
+++ deepinstinctagentless.py
@@ -1,20 +1,18 @@
 # Disclaimer:
 # This code is provided as an example of how to build code against and interact
-# with the Deep Instinct REST API. It is provided AS-IS/NO WARRANTY. It has
-# limited error checking and logging, and likely contains defects or other
-# deficiencies. Test thoroughly first, and use at your own risk. The API
-# Wrapper and associated samples are not Deep Instinct commercial products and
-# are not officially supported, although he underlying REST API is. This means
-# that to report an issue to tech support you must remove the API Wrapper layer
-# and recreate the problem with a reproducible test case against the raw/pure
-# DI REST API.
+# with the Deep Instinct Agentless Connector over REST API. It is provided
+# AS-IS/NO WARRANTY. It has limited error checking and logging, and likely 
+# contains defects or other deficiencies. Test thoroughly first, and use at your 
+# own risk. This sample is not a Deep Instinct commercial product and is not
+# officially supported, although the API that it calls is.
 #
 
 import requests, base64, json, urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-#define varibable for storing FQDN/IP of the agentless connector used for scan
+#varibable for storing FQDN/IP of the agentless connector for performing scan
 agentless_connector = ''
+
 
 def scan_file(file_name, encoded=False):
     # read file from disk. rb means opens the file in binary format for reading
@@ -25,17 +23,17 @@ def scan_file(file_name, encoded=False):
         f.close()
 
     if encoded:
-        #encode file
+        #encode data and set URL to match
         data = base64.b64encode(data)
-        # set request url
         request_url = f'https://{agentless_connector}:5000/scan/base64'
     else:
-        # set request url
+        #leave data as-is and set URL to match
         request_url = f'https://{agentless_connector}:5000/scan/binary'
 
     # send scan request, capture response
     response = requests.post(request_url, data=data, timeout=20, verify=False)
 
+    # validate response code, return verdict as Python dictionary
     if response.status_code == 200:
         verdict = response.json()
         return verdict
@@ -45,5 +43,5 @@ def scan_file(file_name, encoded=False):
         return None
 
 
-def scan_file_encoded (file_name):
+def scan_file_encoded(file_name):
     return scan_file(encoded=True, file_name=file_name)
