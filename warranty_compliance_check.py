8daa052107fa2740f2de7edc8e9ce8611333ee83
pvz01 <81989793+pvz01@users.noreply.github.com>
2021-07-08 08:51:04 -0500

    Update and rename ransomware_warranty_compliance_check.py to warranty_compliance_check.py

--- warranty_compliance_check.py
+++ warranty_compliance_check.py
@@ -1,12 +1,12 @@
 import deepinstinct30 as di  #Import appropriate version of REST API Wrapper from https://github.com/pvz01/deepinstinct-rest-api-wrapper/ that matches D-Appliance version
 import datetime
-​
+
 di.key = 'foo'   #Requires a REST API key with minimum Read Only privileges.
 di.fqdn = 'bar'
-​
+
 #Get all available policy data
 policies = di.get_policies(include_policy_data=True)
-​
+
 #Iterate through policies and determine compliance or lack thereof
 for policy in policies:
     
@@ -36,20 +36,19 @@ for policy in policies:
         #   2. Malicious PowerShell Prevention (compliance requires that this be in Prevention)
         #Both of above require that product make these fields visible via the REST API. Submitted
         #as FR-166 and FR-177 on 2021-07-06
-​
+
 #Print results
-​
+
 print('Deep Instinct Ransomware Warranty Compliance Check |', di.fqdn, '|', datetime.datetime.today().strftime("%Y-%m-%d_%H.%M"))
-​
+
 print('\nThe following Windows policies ARE compliant:')
 for policy in policies:
     if policy['os'] == 'WINDOWS':
         if policy['compliant']:
             print(policy['msp_id'], policy['id'], policy['name'])
-​
+
 print('\nThe following Windows policies ARE NOT compliant:')
 for policy in policies:
     if policy['os'] == 'WINDOWS':
         if not policy['compliant']:
             print(policy['msp_id'], policy['id'], policy['name'], '\nViolations:',  policy['compliance_violations'], '\n')
-    
